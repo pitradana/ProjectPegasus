fc2c8d2a27f1f7c53110fb1ff8fb5da04d63b5e3
Antonius Pitra Dana Arista <pitradana@users.noreply.github.com>
2023-02-21 09:29:25 +0700

    Comparison

--- JAVASCRIPT/main.js
+++ JAVASCRIPT/main.js
@@ -1,1 +1,122 @@
-console.log("Hello World External")
+// console.log("Hello World External")
+
+// console.log(varVariable);
+// var varVariable = "Hello from Var Below";
+// console.log(varVariable);
+// var varVariable = "Hello from Var again";
+// console.log(varVariable);
+
+// let letVariable = "Hellow from Let"
+// console.log(letVariable)
+// letVariable = "Hello from let again"
+// console.log(letVariable)
+
+// const constVariable = "Hello from const"
+// console.log(constVariable)
+// constVariable = "Hellow from const below"
+// console.log(constVariable)
+// constVariable = "Hello from const again"
+// console.log(constVariable)
+
+// let undefinedValue
+// console.log(undefinedValue)
+
+// let nullValue = null
+// console.log(nullValue)
+
+// let numberValue = 23
+// console.log(typeof numberValue)
+
+// let stringValue = "Hello"
+// console.log(typeof stringValue)
+
+// let booleanValue = true
+// console.log(booleanValue)
+// booleanValue = false
+// console.log(booleanValue)
+
+// let objectValue = {
+//     key: "value",
+//     a: 1
+// }
+// console.log(objectValue)
+// console.log(objectValue.a)
+
+// ARithmatic Operators
+
+// let num1 = 2, num2 = 3, num3 = 5
+
+// console.log(num1 + num2)
+// console.log(10 + 5)
+
+// console.log(10 - 5)
+// console.log(num3 - num1)
+
+// console.log(5 * 10)
+// console.log(num2 * num3)
+
+// console.log(8 / 2)
+// console.log(num3 / num1)
+
+// // pangkat
+// console.log(2 ** 2)
+// console.log(num3 ** 2)
+
+// // % modulus
+// console.log(6 % 2)
+// console.log(num3 % num1)
+
+// // ++ increment
+// console.log(num1++) //2, ditampilkan dulu baru tambah
+// console.log(num1) // tampilkan hasilnya
+// console.log(++num1) // tambah dulu baru ditampilkan
+
+// // -- decrement
+// console.log(num3--)
+// console.log(num3)
+// console.log(--num3)
+
+// num1 += num2
+// console.log(num1)
+
+// let num1 = 10, num2 = 50;
+
+// console.log("Belajar Conditional")
+
+// if (num1 > num2) {
+//     console.log("Benar")
+// } else if (num1 < num2) {
+//     console.log("Kondisi if else benar")
+// } else {
+//     console.log("Kondisi seluruh if salah")
+// }
+
+let var1 = 10, var2 = "10"
+
+// if (var1 == var2) {
+//     console.log("if 1 benar")
+// }
+
+// if (var1 === var2) { // === cek tipedata
+//     console.log("if 2 benar")
+// } else {
+//     console.log("if 2 salah")
+// }
+
+// if (var1 != var2) {
+//     console.log("If 1 benar")
+// }
+
+// if (var1 !== var2) {
+//     console.log("If 2 benar")
+// }
+
+let var3 = 25, var4 = 25
+
+if (var3 >= var4) {
+    console.log("If 1 benar")
+}
+
+if (var3 <= var4) {
+    console.log("If 2 benar")
+}
