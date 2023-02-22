9cca77457bebcbda3e31f26f04e8c1a40c6489e2
Antonius Pitra Dana Arista <pitradana@users.noreply.github.com>
2023-02-22 14:29:31 +0700

    DOM Event

--- JAVASCRIPT/dom.js
+++ JAVASCRIPT/dom.js
@@ -20,10 +20,32 @@
 // input.style.borderColor = "red"
 // input.style.color = "blue"
 
-const pElement = document.createElement("p")
-const divEl = document.getElementById("div-1")
+// const pElement = document.createElement("p")
+// const divEl = document.getElementById("div-1")
 
-divEl.appendChild(pElement)
-pElement.innerHTML = "Hello this is P"
+// divEl.appendChild(pElement)
+// pElement.innerHTML = "Hello this is P"
 
-divEl.removeChild(pElement)
+// divEl.removeChild(pElement)
+
+// const hello = document.getElementById("hello")
+
+// hello.addEventListener("mouseenter", function() {
+//     // console.log("Mouse enter hello world text")
+//     hello.style.border = "1px solid red"
+// })
+
+// const btn = document.getElementById("btn")
+// btn.addEventListener("click", function() {
+//     const boom = document.getElementById("boom")
+//     boom.style.display = "block"
+// })
+
+function showBoom () {
+    const boom = document.getElementById("boom")
+    boom.style.display = "block"
+}
+
+function addBorder (el) {
+    el.style.border = "1px solid blue"
+}
