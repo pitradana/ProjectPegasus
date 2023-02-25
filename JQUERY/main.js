28d7aa12482e216a71a2b47391654af3c6bb758b
Antonius Pitra Dana Arista <pitradana@users.noreply.github.com>
2023-02-25 21:10:01 +0700

    jquery done

--- JQUERY/main.js
+++ JQUERY/main.js
@@ -3,7 +3,68 @@
 // const el2 = document.querySelectorAll("p")
 
 // const el = $("#hello")
-const el = $(".world")
+// const el = $(".world")
 
-console.log(el)
+// console.log(el)
 
+// .html()
+// const el = $("#text")
+// el.html("<h1>Hello World</h1>")
+
+// .attr()
+// const inp = $("input")
+// inp.attr("Type","checkbox")
+
+// add & remove class
+// el.addClass("red")
+// el.addClass("blue")
+// el.removeClass()
+
+// css()
+// inp.css({
+//     border: "1px solid red",
+//     marginLeft: "100px"
+// })
+
+// const el = document.getElementById("text")
+// el.addEventListener("mouseenter", function() {
+//     el.style.border = "1px solid red"
+// })
+
+// const el = $("#text")
+// el.mouseenter(function() {
+//     el.css("border", "1px solid red")
+// })
+
+// const inp = $("input")
+// inp.focus(function() {
+//     inp.css("border", "1px solid blue")
+// })
+
+const btnShow = $("#show")
+const btnHide = $("#hide")
+const btnFadeIn = $("#fadeIn")
+const btnFadeOut = $("#fadeOut")
+const btnToggle = $("#toggle")
+
+const div = $("div")
+
+btnShow.click(function() {
+    div.show("slow")
+})
+
+btnHide.click(function() {
+    div.hide("slow")
+})
+
+btnFadeIn.click(function(){
+    div.fadeIn("slow")
+})
+
+btnFadeOut.click(function() {
+    div.fadeOut("slow")
+})
+
+btnToggle.click(function() {
+    div.slideToggle()
+})
