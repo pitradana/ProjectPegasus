63bab2ece5fe51a53b2b7e387820be3bdb4147b9
Antonius Pitra Dana Arista <pitradana@users.noreply.github.com>
2023-02-25 10:34:22 +0700

    Dom Selection JQ

---
+++ JQUERY/main.js
@@ -0,0 +1,9 @@
+// const el = $("p")
+// const el1 = document.getElementsByTagName("p")
+// const el2 = document.querySelectorAll("p")
+
+// const el = $("#hello")
+const el = $(".world")
+
+console.log(el)
+
